abde03be673bc48035362f7ca84c850150ae111e
akshitkrnagpal <akshitkrnagpal@gmail.com>
2020-02-16 22:01:33 +0530

    Fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'expo/build/Expo.fx';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import { activateKeepAwake } from 'expo-keep-awake';
 
-import app from './app/index';
+import app from './src/index';
 
 __DEV__ && activateKeepAwake();
 
