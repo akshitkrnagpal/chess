59822c9f635af23d4d84ae46742200e55af2e829
akshitkrnagpal <akshitkrnagpal@gmail.com>
2022-06-01 05:23:48 +0530

    Update images from chess.com

--- src/images/pieces/index.ts
+++ src/images/pieces/index.ts
@@ -3,13 +3,13 @@ const PieceImages = {
     BQ: require('./black_queen.png'),
     BP: require('./black_pawn.png'),
     BK: require('./black_king.png'),
-    BN: require('./black_horse.png'),
+    BN: require('./black_knight.png'),
     BB: require('./black_bishop.png'),
     WR: require('./white_rook.png'),
     WQ: require('./white_queen.png'),
     WP: require('./white_pawn.png'),
     WK: require('./white_king.png'),
-    WN: require('./white_horse.png'),
+    WN: require('./white_knight.png'),
     WB: require('./white_bishop.png'),
 };
 
